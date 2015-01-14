a7e346559cf2b1e8a3afbe3ae65b2a1b02b76a3b
fundon <cfddream@gmail.com>
2015-01-15 02:37:54 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var to5 = require('gulp-6to5');
+
+gulp.task('default', function () {
+ return gulp.src('src/**/*.js')
+   .pipe(to5({
+     optional: ['coreAliasing'],
+     experimental: true,
+     playground: true
+   }))
+  .pipe(gulp.dest('lib'));
+});
