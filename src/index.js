1b0e46c48ca3c117a414ff7b9b81196f8bcea3c3
fundon <cfddream@gmail.com>
2015-01-16 16:19:51 +0800

    improve

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import RouteSet from './route_set';
 export let Mapper = Mapper;
 export let RouteSet = RouteSet;
 export let HTTP_METHODS = ['get', 'head', 'post', 'patch', 'put', 'delete', 'options'];
+
+class RouteMapper {
+}
+
+export default RouteMapper;
